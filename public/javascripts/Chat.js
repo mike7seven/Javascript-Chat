a69e8f02455c6312ceb7a8220a0f574ce3632029
mike7seven <mikewill7seven@gmail.com>
2017-04-11 18:10:33 -0600

    Missing #
    Missing #on Chat

--- public/javascripts/Chat.js
+++ public/javascripts/Chat.js
@@ -1,21 +1,21 @@
 var socket = io();
 
-//
-//Emit chat message enter key is pressed
-//
+//-----------------------------------------------------------------------------
+// Emit chat message when enter key is pressed.
+//-----------------------------------------------------------------------------
 $("#chat-input").keydown(function(event) {
-    if (event.keyCode == 13) {
-        event.preventDefault();
-        if ($("#chat-input").val() != "") {
-            socket.emit("chat-message", $("#chat-input").val());
-            $("#chat-input").val("");
-        }
-    }
+      if (event.keyCode == 13) {
+          event.preventDefault();
+          if ($("#chat-input").val() != "") {
+              socket.emit("chat-message", $("#chat-input").val());
+              $("#chat-input").val("");
+          }
+      }
 });
 
-//
-//Receive chat message from server.
-//
-socket.on("chat-message"), function(message) {
-    $("chat-container").append(message + "<br />")
-};
+//-----------------------------------------------------------------------------
+// Receive chat message from server.
+//-----------------------------------------------------------------------------
+socket.on("chat-message", function(message) {
+    $("#chat-container").append(message + "<br />")
+});
